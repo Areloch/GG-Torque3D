4fa1027f328b216d556fecedb4b8779abed1aef9
Areloch <areloch@gmail.com>
2019-08-19 23:40:05 -0500

    Added test of Material Complexity Visualizer

--- Engine/source/renderInstance/debug/materialComplexityVisualizerBin.cpp
+++ Engine/source/renderInstance/debug/materialComplexityVisualizerBin.cpp
@@ -19,7 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
-#include "renderInstance/forcedMaterialMeshMgr.h"
+#include "renderInstance/debug/materialComplexityVisualizerBin.h"
 #include "gfx/gfxTransformSaver.h"
 #include "gfx/gfxPrimitiveBuffer.h"
 #include "gfx/gfxDebugEvent.h"
@@ -29,79 +29,95 @@
 #include "console/consoleTypes.h"
 #include "math/util/matrixSet.h"
 
-IMPLEMENT_CONOBJECT(ForcedMaterialMeshMgr);
+IMPLEMENT_CONOBJECT(MaterailComplexityVisualizerBin);
 
-ConsoleDocClass( ForcedMaterialMeshMgr,
+ConsoleDocClass(MaterailComplexityVisualizerBin,
    "@brief Basically the same as RenderMeshMgr, but will override the material "
    "of the instance. Exists for backwards compatibility, not currently used, soon to be deprecated\n\n"
    "@internal"
 );
 
-ForcedMaterialMeshMgr::ForcedMaterialMeshMgr()
+MaterailComplexityVisualizerBin::MaterailComplexityVisualizerBin()
 {
    mOverrideInstance = NULL;
    mOverrideMaterial = NULL;
+   mMaxComplexity = 16;
 }
 
-ForcedMaterialMeshMgr::ForcedMaterialMeshMgr(RenderInstType riType, F32 renderOrder, F32 processAddOrder, BaseMatInstance* overrideMaterial)
-: RenderMeshMgr(riType, renderOrder, processAddOrder)
+MaterailComplexityVisualizerBin::MaterailComplexityVisualizerBin(RenderInstType riType, F32 renderOrder, F32 processAddOrder, BaseMatInstance* overrideMaterial)
+   : RenderMeshMgr(riType, renderOrder, processAddOrder)
 {
    mOverrideInstance = overrideMaterial;
    mOverrideMaterial = NULL;
+   mMaxComplexity = 16;
 }
 
-void ForcedMaterialMeshMgr::setOverrideMaterial(BaseMatInstance* overrideMaterial)
+void MaterailComplexityVisualizerBin::setOverrideMaterial(BaseMatInstance* overrideMaterial)
 {
    SAFE_DELETE(mOverrideInstance);
    mOverrideInstance = overrideMaterial;
 }
 
-ForcedMaterialMeshMgr::~ForcedMaterialMeshMgr()
+MaterailComplexityVisualizerBin::~MaterailComplexityVisualizerBin()
 {
    setOverrideMaterial(NULL);
 }
 
-void ForcedMaterialMeshMgr::initPersistFields()
+void MaterailComplexityVisualizerBin::initPersistFields()
 {
-   addProtectedField("material", TYPEID< Material >(), Offset(mOverrideMaterial, ForcedMaterialMeshMgr),
+   addProtectedField("material", TYPEID< Material >(), Offset(mOverrideMaterial, MaterailComplexityVisualizerBin),
       &_setOverrideMat, &_getOverrideMat, "Material used to draw all meshes in the render bin.");
+
+   addField("maxComplexity", TypeF32, Offset(mMaxComplexity, MaterailComplexityVisualizerBin), "");
 
    Parent::initPersistFields();
 }
 
-void ForcedMaterialMeshMgr::render(SceneRenderState * state)
+void MaterailComplexityVisualizerBin::render(SceneRenderState* state)
 {
-   PROFILE_SCOPE(ForcedMaterialMeshMgr_render);
+   PROFILE_SCOPE(MaterailComplexityVisualizerBin_render);
 
-   if(!mOverrideInstance && mOverrideMaterial.isValid())
+   if (!mOverrideInstance && mOverrideMaterial.isValid())
    {
       mOverrideInstance = mOverrideMaterial->createMatInstance();
-      mOverrideInstance->init( MATMGR->getDefaultFeatures(), getGFXVertexFormat<GFXVertexPNTBT>() );
+      mOverrideInstance->init(MATMGR->getDefaultFeatures(), getGFXVertexFormat<GFXVertexPNTBT>());
    }
 
    // Early out if nothing to draw.
-   if(!mElementList.size() || !mOverrideInstance)
+   if (!mElementList.size() || !mOverrideInstance)
       return;
 
-   GFXDEBUGEVENT_SCOPE(ForcedMaterialMeshMgr_Render, ColorI::RED);
+   GFXDEBUGEVENT_SCOPE(MaterailComplexityVisualizerBin_Render, ColorI::RED);
 
    // Automagically save & restore our viewport and transforms.
    GFXTransformSaver saver;
 
    // init loop data
    SceneData sgData;
-   sgData.init( state );
+   sgData.init(state);
 
-   MeshRenderInst *ri = static_cast<MeshRenderInst*>(mElementList[0].inst);
-   setupSGData( ri, sgData );
+   MeshRenderInst* ri = static_cast<MeshRenderInst*>(mElementList[0].inst);
+   setupSGData(ri, sgData);
 
    while (mOverrideInstance->setupPass(state, sgData))
-   {   
-      for( U32 j=0; j<mElementList.size(); j++)
+   {
+      //set the material shader const
+      MaterialParameters* matParams = mOverrideInstance->getMaterialParameters();
+      MaterialParameterHandle* complexitySC = mOverrideInstance->getMaterialParameterHandle("$materialComplexity");
+      MaterialParameterHandle* minComplexitySC = mOverrideInstance->getMaterialParameterHandle("$minComplexity");
+      MaterialParameterHandle* maxComplexitySC = mOverrideInstance->getMaterialParameterHandle("$maxComplexity");
+
+      for (U32 j = 0; j < mElementList.size(); j++)
       {
          MeshRenderInst* passRI = static_cast<MeshRenderInst*>(mElementList[j].inst);
-         if(passRI->primBuff->getPointer()->mPrimitiveArray[passRI->primBuffIndex].numVertices < 1)
+         if (passRI->primBuff->getPointer()->mPrimitiveArray[passRI->primBuffIndex].numVertices < 1)
             continue;
+
+         //Calculate a value based on the material for the renderInst
+         BaseMatInstance* originalMat = passRI->matInst;
+         float featureCount = originalMat->getFeatures().getCount();
+
+         matParams->setSafe(complexitySC, featureCount);
 
          getRenderPass()->getMatrixSet().setWorld(*passRI->objectToWorld);
          getRenderPass()->getMatrixSet().setView(*passRI->worldToCamera);
@@ -109,25 +125,28 @@ void ForcedMaterialMeshMgr::render(SceneRenderState * state)
          mOverrideInstance->setTransforms(getRenderPass()->getMatrixSet(), state);
 
          mOverrideInstance->setBuffers(passRI->vertBuff, passRI->primBuff);
-         GFX->drawPrimitive( passRI->primBuffIndex );                  
+         GFX->drawPrimitive(passRI->primBuffIndex);
       }
+
+      matParams->setSafe(minComplexitySC, 5.0f);
+      matParams->setSafe(maxComplexitySC, mMaxComplexity);
    }
 }
 
-const char* ForcedMaterialMeshMgr::_getOverrideMat( void *object, const char *data )
+const char* MaterailComplexityVisualizerBin::_getOverrideMat(void* object, const char* data)
 {
-   ForcedMaterialMeshMgr &mgr = *reinterpret_cast<ForcedMaterialMeshMgr *>( object );
-   if( mgr.mOverrideMaterial.isValid() )
+   MaterailComplexityVisualizerBin& mgr = *reinterpret_cast<MaterailComplexityVisualizerBin*>(object);
+   if (mgr.mOverrideMaterial.isValid())
       return mgr.mOverrideMaterial->getIdString();
    else
       return "0";
 }
 
-bool ForcedMaterialMeshMgr::_setOverrideMat( void *object, const char *index, const char *data )
+bool MaterailComplexityVisualizerBin::_setOverrideMat(void* object, const char* index, const char* data)
 {
-   ForcedMaterialMeshMgr &mgr = *reinterpret_cast<ForcedMaterialMeshMgr *>( object );   
+   MaterailComplexityVisualizerBin& mgr = *reinterpret_cast<MaterailComplexityVisualizerBin*>(object);
    Material* material;
-   Sim::findObject( data, material );
+   Sim::findObject(data, material);
    mgr.mOverrideMaterial = material;
 
    return false;
